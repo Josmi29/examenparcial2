b30b4c67444a696b1c7d5af6033343ecd52883ad
Josmi29 <141948673+Josmi29@users.noreply.github.com>
2023-10-03 11:40:02 -0600

    Add files via upload

---
+++ ejercicio3examen2.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+//codigo que genera una piramide en n numero de lineas
+int main(){
+	int i, j, lineas;
+	
+	printf("dime un num. de lineas para la piramide: ");
+	scanf("%d", &lineas);
+	
+	for(i=1;i<=lineas;i++)
+	{
+		for(j=1;j<=i;j++)
+		printf("%d ", j);
+		printf("\n");
+	}
+	
+	
+	
+	return 0;
+}
