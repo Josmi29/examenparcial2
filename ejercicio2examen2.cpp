d7244c4ba05b078b95f3504b9cc1849808e92db4
Josmi29 <141948673+Josmi29@users.noreply.github.com>
2023-10-03 10:37:05 -0600

    Add files via upload

---
+++ ejercicio2examen2.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+//multiplos del 3
+int main() {
+	int i;
+	
+    for (i = 1; i <= 100; i++) 
+	{
+        if (i % 3 == 0) 
+		{
+            printf("%d es multiplo de tres\n", i);
+        } 
+		else 
+		{
+            printf("%d no es multiplo de tres\n", i);
+        }
+    }
+
+    return 0;
+}
+
