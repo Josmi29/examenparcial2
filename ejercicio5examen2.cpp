d1a9e3208957a73184fe1ce66ac2085ef3d39970
Josmi29 <141948673+Josmi29@users.noreply.github.com>
2023-10-03 10:38:45 -0600

    Add files via upload

---
+++ ejercicio5examen2.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+//codigo que te dice si un año es bisiesto o no
+int main(){
+	int a=0;
+	
+	printf("introduzca año:\n");
+	scanf("%d", &a);
+	
+	if( (a % 4 == 0) && (a % 100 != 0) || a % 400 == 0 )
+	{
+		printf("si es bisiesto:)");
+	} 
+	else 
+	{
+		printf("no es bisiesto:0");
+	}
+	
+	return 0;
+}
