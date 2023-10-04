abc81897f8f9c6059201f17434ee77a74caab662
Josmi29 <141948673+Josmi29@users.noreply.github.com>
2023-10-04 10:38:01 -0600

    Update ejercicio4examen2.cpp

--- ejercicio4examen2.cpp
+++ ejercicio4examen2.cpp
@@ -1,27 +1,27 @@
 #include <stdio.h>
-//programa que intercmbia valores
-//jose miguel ruiz marquez 180200
-int main() {
-    int v1, v2, v3;
 
-    printf("escribe el valor del primer num: ");
+int main() 
+{
+    int v1, v2, v3;
+    
+    printf("escribe v1: ");
     scanf("%d", &v1);
 
-    printf("escribe el valor del segundo num: ");
+    printf("escribe v2: ");
     scanf("%d", &v2);
 
-    printf("escribe el valor del tercer num: ");
+    printf("escribe v3: ");
     scanf("%d", &v3);
-
-    printf("valores originales: %d, %d, %d\n", v1, v2, v3);
-
  
     int temp = v1;
-    v1 = v2;
-    v2 = v3;
-    v3 = temp;
-
-    printf("valores intercambiados: %d, %d, %d\n", v1, v2, v3);
+    v1 = v3;
+    v3 = v2;
+    v2 = temp;
+  
+    printf("valores intercambiados:\n");
+    printf("v1 = %d\n", v1);
+    printf("v2 = %d\n", v2);
+    printf("v3 = %d\n", v3);
 
     return 0;
 }
